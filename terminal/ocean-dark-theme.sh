a23f592a76e95a1f2bd7697d48c23beea0e28e35
Vasiliy Spodin <v@spodin.com>
2017-03-20 22:46:17 +0200

    Improve theme and custom prompt

--- terminal/ocean-dark-theme.sh
+++ terminal/ocean-dark-theme.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-## Set up colors, cursor color and shape
-gsettings set org.pantheon.terminal.settings palette '#2b2f3a:#bf616a:#ee8e96:#fbda9f:#8fa1b3:#b48ead:#96b5b4:#a3a4b5:#65737e:#bf616a:#ee8e96:#fbda9f:#8fa1b3:#b48ead:#96b5b4:#eff1f5'
-gsettings set org.pantheon.terminal.settings background '#2b2f3a'
-gsettings set org.pantheon.terminal.settings foreground '#a3a4b5'
-gsettings set org.pantheon.terminal.settings cursor-color '#a3a4b5'
-gsettings set org.pantheon.terminal.settings cursor-shape 'I-Beam'
-gsettings set org.pantheon.terminal.settings tab-bar-behavior 'Hide When Single Tab'
+## Set up colors, cursor color and shape2
+gsettings set org.pantheon.terminal.settings palette '#2b303b:#bf616a:#a3be8c:#ebcb8b:#8fa1b3:#b48ead:#96b5b4:#c0c5ce:#65737e:#bf616a:#a3be8c:#ebcb8b:#8fa1b3:#b48ead:#96b5b4:#eff1f5'
+gsettings set org.pantheon.terminal.settings background '#2b303b'
+gsettings set org.pantheon.terminal.settings foreground '#c0c5ce'
+gsettings set org.pantheon.terminal.settings cursor-color '#c0c5ce'
+gsettings get org.pantheon.terminal.settings cursor-shape 'I-Beam'
+gsettings get org.pantheon.terminal.settings tab-bar-behavior 'Hide When Single Tab'
 
 ## Elementary OS default properties (uncomment if necessary)
 #gsettings set org.pantheon.terminal.settings palette '#073642:#dc322f:#859900:#b58900:#268bd2:#ec0048:#2aa198:#94a3a5:#586e75:#cb4b16:#859900:#b58900:#268bd2:#d33682:#2aa198:#6c71c4'
