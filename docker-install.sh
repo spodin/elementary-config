60aa9eca9b951c49bee1027e9600dc7ccdc66f7d
Vasiliy Spodin <v@spodin.com>
2017-06-29 17:37:44 +0300

    Suppress confirmation on additional packages installation

--- docker-install.sh
+++ docker-install.sh
@@ -8,7 +8,7 @@
 sudo apt-get update
 
 ## Install packages to allow apt to use a repository over HTTPS
-sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
 
 ## Add Docker’s official GPG key
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
