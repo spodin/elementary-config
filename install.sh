e1db76073dd3fa4962b7b59a6933ea9d8e18846e
Vasiliy Spodin <v@spodin.engineer>
2018-02-02 17:49:46 +0200

    Add Docker CE and Docker Compose installation scripts

--- install.sh
+++ install.sh
@@ -120,6 +120,22 @@ sudo apt-get install transmission -y
 wget -O postman.tar.gz https://dl.pstmn.io/download/latest/linux64
 tar xf postman.tar.gz -C ~/workspace
 
+# DOCKER & DOCKER COMPOSE
+# For Docker Compose you should verify latest release version at:
+# https://github.com/docker/compose/releases
+# ---------------------------------------------------------------
+
+## Docker CE (https://docs.docker.com/install/linux/docker-ce/ubuntu/#install-using-the-convenience-script)
+curl -fsSL get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
+## Docker Compose (https://docs.docker.com/compose/install/#master-builds)
+#sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+#sudo chmod +x /usr/local/bin/docker-compose
+
+## Docker Compose Command-line completion ()
+#sudo curl -L https://raw.githubusercontent.com/docker/compose/1.18.0/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+
 # ASSETS INSTALLATION
 # -------------------
 
