9cdcbf8d3ff58a0afd74cdec8aea43bcf8e1b853
Vasiliy Spodin <v@spodin.com>
2019-09-26 22:24:37 +0300

    Reorder commands

--- install.sh
+++ install.sh
@@ -1,39 +1,53 @@
 #!/bin/bash
 
-## Purge unused applications
 sudo apt-get purge pantheon-mail -y
 sudo apt-get purge audience -y
 sudo apt-get purge simple-scan -y
 sudo apt-get purge epiphany-browser -y
 
-## System Update
 sudo apt-get update && sudo apt-get dist-upgrade
 
-## Gdebi
+sudo apt-get install software-properties-common -y
+sudo apt-get install software-properties-gtk -y
+sudo apt-get install ppa-purge -y
+
 sudo apt install gdebi -y
 
-## Software Properties Common to enable PPA
-sudo apt-get install software-properties-common -y
+## Git (https://git-scm.com)
+sudo add-apt-repository ppa:git-core/ppa -y
+sudo apt-get install git -y
+sudo apt-get install tig -y
 
-## Software & Updates Application
-sudo apt-get install software-properties-gtk
-
-## PPA Purge
-sudo apt-get install ppa-purge
-
-## Seahorse: Passwords and Encryption Keys Manager (https://wiki.gnome.org/Apps/Seahorse)
-sudo apt-get install seahorse
+## Z shell (Zsh)
+sudo apt-get install zsh
+sudo apt-get install powerline fonts-powerline
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+chsh -s /bin/zsh
 
 ## Google Chrome (https://www.google.com/chrome)
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo gdebi --non-interactive google-chrome-stable_current_*.deb
 
-## Git (https://git-scm.com)
-sudo add-apt-repository ppa:git-core/ppa -y
-sudo apt-get install git -y
+## Midnight Commander (https://midnight-commander.org)
+sudo add-apt-repository ppa:eugenesan/ppa -y
+sudo apt-get install mc -y
 
-## Text-mode Interface for Git (https://github.com/jonas/tig)
-sudo apt-get install tig -y
+## Double Commander (http://doublecmd.sourceforge.net)
+sudo add-apt-repository ppa:alexx2000/doublecmd -y
+sudo apt-get install doublecmd-gtk -y
+
+## XClip (https://github.com/astrand/xclip)
+sudo apt-get install xclip -y
+
+## Elementary Tweaks (https://github.com/elementary-tweaks/elementary-tweaks)
+sudo add-apt-repository ppa:philip.scott/elementary-tweaks -y
+sudo apt-get install elementary-tweaks -y
+
+## LXTask Task Manager
+sudo apt-get install lxtask -y
+
+## Seahorse: Passwords and Encryption Keys Manager (https://wiki.gnome.org/Apps/Seahorse)
+sudo apt-get install seahorse -y
 
 ## Apache Maven (https://maven.apache.org)
 sudo apt-get install maven -y
@@ -45,14 +59,6 @@ sudo apt-get install gradle-ppa -y
 ## DBeaver (https://dbeaver.jkiss.org/download)
 wget -O dbeaver-ce.tar.gz https://dbeaver.jkiss.org/files/dbeaver-ce-latest-linux.gtk.x86_64.tar.gz
 tar xf dbeaver-ce.tar.gz -C ~/workspace
-
-## Double Commander (http://doublecmd.sourceforge.net)
-sudo add-apt-repository ppa:alexx2000/doublecmd -y
-sudo apt-get install doublecmd-gtk -y
-
-## Midnight Commander (https://midnight-commander.org)
-sudo add-apt-repository ppa:eugenesan/ppa -y
-sudo apt-get install mc -y
 
 ## Github Atom (https://atom.io)
 sudo add-apt-repository ppa:webupd8team/atom -y
@@ -87,16 +93,6 @@ sudo apt-get install vlc -y
 sudo wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl
 sudo chmod a+rx /usr/local/bin/youtube-dl
 
-## XClip (https://github.com/astrand/xclip)
-sudo apt-get install xclip -y
-
-## Elementary Tweaks (https://github.com/elementary-tweaks/elementary-tweaks)
-sudo add-apt-repository ppa:philip.scott/elementary-tweaks -y
-sudo apt-get install elementary-tweaks -y
-
-## LXTask Task Manager
-sudo apt-get install lxtask -y
-
 ## Transmission BitTorrent Client (https://transmissionbt.com)
 sudo apt-get install transmission -y
 
@@ -106,12 +102,6 @@ sudo apt-get install httpie -y
 ## A cat(1) clone with syntax highlighting and Git integration (https://github.com/sharkdp/bat)
 wget https://github.com/sharkdp/bat/releases/download/v0.12.1/bat_0.12.1_amd64.deb
 sudo gdebi bat*
-
-## Z shell (Zsh)
-sudo apt-get install zsh
-sudo apt-get install powerline fonts-powerline
-git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-chsh -s /bin/zsh
 
 ## Postman (https://www.getpostman.com)
 #wget -O postman.tar.gz https://dl.pstmn.io/download/latest/linux64
